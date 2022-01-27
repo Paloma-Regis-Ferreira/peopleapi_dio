561c1bc59f706da4621515ebd20e59de80a11b9b
Paloma-Regis-Ferreira <palomaregis29@gmail.com>
2022-01-27 17:38:37 -0300

    implementacao do DTO

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.response.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,10 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = Person.builder().firstName(personDTO.getFirstName()).lastName(personDTO.getLastName()).birthDate(personDTO.getBirthDate()).phones(personDTO.getPhones()).build();
+
+        Person savedPerson = personRepository.save(personDTO);
         return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
     }
 }
